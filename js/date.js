1eb472ddbb93c2a99ae32b041a085aea38337d2a
Dragonogs <oliver_shipley@yahoo.co.uk>
2024-08-13 01:45:37 +0100

    about

--- js/date.js
+++ js/date.js
@@ -1,1 +1,7 @@
 document.querySelector(".year").textContent = new Date().getFullYear();
+
+const age = Math.floor(
+  (new Date() - new Date("2001-04-26").getTime()) / 3.15576e10
+);
+
+document.querySelector(".age").textContent = age;
